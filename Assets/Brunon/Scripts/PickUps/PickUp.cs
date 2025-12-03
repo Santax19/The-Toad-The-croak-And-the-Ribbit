54daf3fa8b36b0c3150778c8410eb1c563221dfa
Santax19 <santielcapo1@gmail.com>
2025-12-03 16:44:05 -0300

    Pickups in network

--- Assets/Brunon/Scripts/PickUps/PickUp.cs
+++ Assets/Brunon/Scripts/PickUps/PickUp.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
-public class Pickup : MonoBehaviour
+public class Pickup : NetworkBehaviour
 {
     [SerializeField] private BuffData _buff;
+
+    private bool _consumedLocal = false;
     public void Consume(PlayerHealth health, MovementController movement)
     {
+        if (Object == null || !Object.IsValid) return;
+        if (_consumedLocal) return;
+
         var weaponManager = health.GetComponent<WeaponManager>();
         if (health != null && movement != null && weaponManager != null)
         {
+            _consumedLocal = true;
+
             _buff.Apply(health, movement, weaponManager);
-            Destroy(gameObject);
+            Runner.Despawn(Object);
         }
     }
 }
